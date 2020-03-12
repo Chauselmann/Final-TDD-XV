01fb92eeacb2805fc1e9f318e81cb93112f76bb7
chauselmann <charlie@cosavostra.com>
2020-03-12 16:41:30 +0100

    RED : Should return the name of the winning team

--- src/Entity/Fight.php
+++ src/Entity/Fight.php
@@ -1,9 +1,39 @@
 <?php
 
+namespace App\Entity;
 
+use function MongoDB\BSON\fromJSON;
 
+/**
+ * Class Fight
+ * @package Fight
+ */
 
 class Fight
 {
+    /** @var string */
+    private $id;
 
+    /** @var array */
+    private $teams;
+
+    /** @var int */
+    private $turn = 0;
+
+    /**
+     * Fight constructor.
+     * @param Team $team1
+     * @param Team $team2
+     */
+    public function __construct($team1, $team2)
+    {
+        $this->id = uniqid();
+        $this->teams[] = $team1;
+        $this->teams[] = $team2;
+    }
+
+    public function start()
+    {
+
+    }
 }
